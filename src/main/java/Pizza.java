30ce9a94d2b743a36676150f34cd63ca570c82c5
moe pordos <pmarneisha@gmail.com>
2025-11-14 13:22:24 -0600

    formatting

--- src/main/java/Pizza.java
+++ src/main/java/Pizza.java
@@ -1,49 +1,49 @@
 import java.util.List;
 
 public class Pizza extends Product {
-        //Small, Medium, Large
-        private final String size;
-        // stuffed or not
-        private boolean stuffedCrust;
-        //regular toppings
-        private List<Topping> toppings;
-//        private Topping toppings;
-        //sauces
+    //Small, Medium, Large
+    private final String size;
+    // stuffed or not
+    private boolean stuffedCrust;
+    //regular toppings
+    private List<Topping> toppings;
+    //        private Topping toppings;
+    //sauces
 //        private List<String> sauces = new ArrayList<>();
-        private String sauce;
+    private String sauce;
 
-        //base price of pizza
-        private double basePrice;
+    //base price of pizza
+    private double basePrice;
 
-        //Constructor, sets instance variables
-        //Initialize list for toppings,meats, and cheese
-        //Calls methods
-        public Pizza(String size, String crustType, boolean stuffedCrust,  String sauce) {
-            this.size = size;
-            //thin, regular, thick, cauliflower
-            this.stuffedCrust = stuffedCrust;
+    //Constructor, sets instance variables
+    //Initialize list for toppings,meats, and cheese
+    //Calls methods
+    public Pizza(String size, String crustType, boolean stuffedCrust, String sauce) {
+        this.size = size;
+        //thin, regular, thick, cauliflower
+        this.stuffedCrust = stuffedCrust;
 //            this.toppings = toppings;
-            this.sauce = sauce;
-            //sets base price based on size
+        this.sauce = sauce;
+        //sets base price based on size
 //        basePrice = setBasePrice();
-        }
+    }
 
-        // base prices according to size
-        private void setBasePrice() {
-            if (size.equalsIgnoreCase("small")) {
-                basePrice = 8.50;
-            } else if (size.equalsIgnoreCase("medium")) {
-                basePrice = 12.00;
-            } else if (size.equalsIgnoreCase("large")) {
-                basePrice = 16.50;
-            } else {
-                basePrice = 0.0;
-                System.out.println("Invalid pizza size.");
-            }
-            if (stuffedCrust) {
-                basePrice += 2.00;
-            }
+    // base prices according to size
+    private void setBasePrice() {
+        if (size.equalsIgnoreCase("small")) {
+            basePrice = 8.50;
+        } else if (size.equalsIgnoreCase("medium")) {
+            basePrice = 12.00;
+        } else if (size.equalsIgnoreCase("large")) {
+            basePrice = 16.50;
+        } else {
+            basePrice = 0.0;
+            System.out.println("Invalid pizza size.");
         }
+        if (stuffedCrust) {
+            basePrice += 2.00;
+        }
+    }
 
     public double calculateTotalPrice() {
         double total = basePrice;
@@ -73,4 +73,5 @@ public class Pizza extends Product {
     public String getSauce() {
         return sauce;
     }
+
 }
